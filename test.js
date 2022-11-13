5164cc62823bdd16fa7a2cced72bd3c53d506e97
0x7844 <0x7844@protonmail.com>
2022-11-13 22:26:53 +0100

    added tests

---
+++ test.js
@@ -0,0 +1,119 @@
+const { Linter } = require( 'eslint' );
+const rules = require( './rules' );
+
+
+
+const linter = new Linter();
+
+const samples = {
+    function: `
+function func( n ) {
+    if ( n < 2 ) {
+        return n;
+    }
+    return func( n - 1 ) + func( n - 2 );
+}
+`,
+    loop: `
+for ( let i = 0; i < 10; i++ ) {
+    console.log( !( i % 3 ) ? 'Fizz' : '' ) + !( i % 5 ) ? 'Buzz' : '' + i;
+}
+`,
+
+    conditional: `
+if ( ~( 2 ** 32 - 1 ) ) {
+    console.log( 'ඞ' );
+} else if ( ~( 2 ** 64 - 1 ) ) { // exeeds Number.MAX_SAFE_INTEGER
+    console.log( '???' );
+} else {
+    console.log( '(^ ^)b' );
+}
+`,
+    switch: `
+switch ( true ) {
+    case false:
+        void 0;
+        break;
+    default:
+        void 1;
+}
+`,
+    'block-scope': `
+let someGlobal = '';
+class Test {
+    constructor() {
+        this.someProperty = '';
+    }
+    someMethod() {
+        return this.someProperty;
+    }
+    get someGetter() {
+        return someGlobal;
+    }
+    set someSetter( value ) {
+        someGlobal = value;
+    }
+}
+function func() {}
+`,
+    keywords: `
+const a = 1;
+const b = 2;
+function test() {
+    let c = 3;
+    return a + b;
+}
+`,
+    contructors: `
+function UpperCaseContructor() {
+    this.someProperty = '';
+    return this;
+}
+`,
+    operators: `
+let i = 0;
+i++;
+i--;
+++i;
+--i;
+i = ~i;
+i = !i;
+i = i + 1;
+`
+};
+
+
+
+console.log( '[eslint-config-noxar]', 'Starting tests...' );
+
+
+let fails = 0;
+
+for ( const [ name, sample ] of Object.entries( samples ) ) {
+
+    const errors = linter.verify( sample, rules ).filter( m => m.severity === 2 /* error */ );
+
+    if ( !errors.length ) {
+
+        console.log( '✓', name );
+        continue;
+
+    }
+
+    console.error( '[eslint-config-noxar]', name + ' failed to pass.' );
+    console.error( ...errors );
+    fails++;
+
+}
+
+
+
+if ( fails ) {
+
+    console.error( '[eslint-config-noxar]', 'Process terminated.' );
+    process.exit( 1 );
+
+}
+
+
+console.info( '[eslint-config-noxar]', 'Process completed.' );
